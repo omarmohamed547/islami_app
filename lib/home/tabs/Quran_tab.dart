4496918096a0db4dbb9781d0ac82e8536ab756aa
omarmohamed547 <ramdan5489@gmail.com>
2024-12-11 21:54:26 +0200

    Added bottom navigation bar. Finished home screen.

--- lib/home/tabs/Quran_tab.dart
+++ lib/home/tabs/Quran_tab.dart
@@ -5,6 +5,6 @@ class QuranTab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Container();
   }
 }
--- lib/home/tabs/Sebha_tab.dart
+++ lib/home/tabs/Sebha_tab.dart
@@ -5,6 +5,6 @@ class SebhaTab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Placeholder();
   }
 }
